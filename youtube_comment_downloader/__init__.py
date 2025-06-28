b720b608c2d8703b1f6d1a20410e75e743a7a0f5
Jo. Adly <youssefadly237@gmail.com>
2025-06-28 04:13:38 +0300

    Fix incorrect comment count when no comments and avoid creating empty output file
    Start counting from 0 to correctly handle zero comments
    
    Check for comments before creating output file to avoid empty files
    
    Apply code formatting for consistency

--- youtube_comment_downloader/__init__.py
+++ youtube_comment_downloader/__init__.py
@@ -14,21 +14,54 @@ def to_json(comment, indent=None):
     comment_str = json.dumps(comment, ensure_ascii=False, indent=indent)
     if indent is None:
         return comment_str
-    padding = ' ' * (2 * indent) if indent else ''
-    return ''.join(padding + line for line in comment_str.splitlines(True))
+    padding = " " * (2 * indent) if indent else ""
+    return "".join(padding + line for line in comment_str.splitlines(True))
 
 
-def main(argv = None):
-    parser = argparse.ArgumentParser(add_help=False, description=('Download Youtube comments without using the Youtube API'))
-    parser.add_argument('--help', '-h', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
-    parser.add_argument('--youtubeid', '-y', help='ID of Youtube video for which to download the comments')
-    parser.add_argument('--url', '-u', help='Youtube URL for which to download the comments')
-    parser.add_argument('--output', '-o', help='Output filename (output format is line delimited JSON)')
-    parser.add_argument('--pretty', '-p', action='store_true', help='Change the output format to indented JSON')
-    parser.add_argument('--limit', '-l', type=int, help='Limit the number of comments')
-    parser.add_argument('--language', '-a', type=str, default=None, help='Language for Youtube generated text (e.g. en)')
-    parser.add_argument('--sort', '-s', type=int, default=SORT_BY_RECENT,
-                        help='Whether to download popular (0) or recent comments (1). Defaults to 1')
+def main(argv=None):
+    parser = argparse.ArgumentParser(
+        add_help=False,
+        description=("Download Youtube comments without using the Youtube API"),
+    )
+    parser.add_argument(
+        "--help",
+        "-h",
+        action="help",
+        default=argparse.SUPPRESS,
+        help="Show this help message and exit",
+    )
+    parser.add_argument(
+        "--youtubeid",
+        "-y",
+        help="ID of Youtube video for which to download the comments",
+    )
+    parser.add_argument(
+        "--url", "-u", help="Youtube URL for which to download the comments"
+    )
+    parser.add_argument(
+        "--output", "-o", help="Output filename (output format is line delimited JSON)"
+    )
+    parser.add_argument(
+        "--pretty",
+        "-p",
+        action="store_true",
+        help="Change the output format to indented JSON",
+    )
+    parser.add_argument("--limit", "-l", type=int, help="Limit the number of comments")
+    parser.add_argument(
+        "--language",
+        "-a",
+        type=str,
+        default=None,
+        help="Language for Youtube generated text (e.g. en)",
+    )
+    parser.add_argument(
+        "--sort",
+        "-s",
+        type=int,
+        default=SORT_BY_RECENT,
+        help="Whether to download popular (0) or recent comments (1). Defaults to 1",
+    )
 
     try:
         args = parser.parse_args() if argv is None else parser.parse_args(argv)
@@ -41,14 +74,16 @@ def main(argv = None):
 
         if (not youtube_id and not youtube_url) or not output:
             parser.print_usage()
-            raise ValueError('you need to specify a Youtube ID/URL and an output filename')
+            raise ValueError(
+                "you need to specify a Youtube ID/URL and an output filename"
+            )
 
         if os.sep in output:
             outdir = os.path.dirname(output)
             if not os.path.exists(outdir):
                 os.makedirs(outdir)
 
-        print('Downloading Youtube comments for', youtube_id or youtube_url)
+        print("Downloading Youtube comments for", youtube_id or youtube_url)
         downloader = YoutubeCommentDownloader()
         generator = (
             downloader.get_comments(youtube_id, args.sort, args.language)
@@ -56,29 +91,43 @@ def main(argv = None):
             else downloader.get_comments_from_url(youtube_url, args.sort, args.language)
         )
 
-        count = 1
-        with io.open(output, 'w', encoding='utf8') as fp:
-            sys.stdout.write('Downloaded %d comment(s)\r' % count)
+        count = 0
+        start_time = time.time()
+        comment = next(generator, None)
+
+        if comment is None:
+            print("No comments found, no file created.")
+            return  # Exit early, no file created
+
+        with io.open(output, "w", encoding="utf8") as fp:
+            sys.stdout.write("Downloaded %d comment(s)\r" % count)
             sys.stdout.flush()
-            start_time = time.time()
 
             if pretty:
-                fp.write('{\n' + ' ' * INDENT + '"comments": [\n')
+                fp.write("{\n" + " " * INDENT + '"comments": [\n')
 
-            comment = next(generator, None)
             while comment:
                 comment_str = to_json(comment, indent=INDENT if pretty else None)
-                comment = None if limit and count >= limit else next(generator, None)  # Note that this is the next comment
-                comment_str = comment_str + ',' if pretty and comment is not None else comment_str
-                print(comment_str.decode('utf-8') if isinstance(comment_str, bytes) else comment_str, file=fp)
-                sys.stdout.write('Downloaded %d comment(s)\r' % count)
-                sys.stdout.flush()
                 count += 1
+                comment = None if limit and count >= limit else next(generator, None)
+                comment_str = (
+                    comment_str + "," if pretty and comment is not None else comment_str
+                )
+                print(
+                    (
+                        comment_str.decode("utf-8")
+                        if isinstance(comment_str, bytes)
+                        else comment_str
+                    ),
+                    file=fp,
+                )
+                sys.stdout.write("Downloaded %d comment(s)\r" % count)
+                sys.stdout.flush()
 
             if pretty:
-                fp.write(' ' * INDENT +']\n}')
-        print('\n[{:.2f} seconds] Done!'.format(time.time() - start_time))
+                fp.write(" " * INDENT + "]\n}")
+        print("\n[{:.2f} seconds] Done!".format(time.time() - start_time))
 
     except Exception as e:
-        print('Error:', str(e))
+        print("Error:", str(e))
         sys.exit(1)
